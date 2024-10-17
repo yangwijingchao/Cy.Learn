09d20efdb4335eec252b3a949108dcebfd223a35
yangchao <135QETadg>
2024-10-17 11:32:38 +0800

    1

--- JwtWebApi/Program.cs
+++ JwtWebApi/Program.cs
@@ -3,13 +3,6 @@ using System.Security.Claims;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-List<string> list = new List<string>();
-list.Add("恭喜您获得《苍穹飞仙》游戏礼包");
-list.Add("苍穹飞仙礼包");
-list.Add("灵宝令:10;炼制碎片:50;强化石:100;");
-list.Add("永久");
-list.Add("礼包码可在游戏中输入领取，您也可以在手机上体验此游戏，点击详情查看\u2193\u2193");
-var json = Newtonsoft.Json.JsonConvert.SerializeObject(list);
 
 builder.Services.AddControllers();
 // 添加身份验证服务
